b36990341ecbabceb445e4ec27e9fe8227d30339
Kanta Tamura <kanta03085@gmail.com>
2021-01-15 23:26:17 +0900

    fix: standard input only

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,9 @@
 use std::io::{stdin, BufRead, Error};
 
 fn main() {
-    let board = vec![
-        vec![0, 2, 0, 0, 7, 5, 0, 1, 0],
-        vec![1, 0, 0, 0, 0, 4, 5, 0, 8],
-        vec![0, 5, 6, 8, 0, 0, 2, 0, 0],
-        vec![8, 1, 0, 0, 0, 0, 7, 0, 0],
-        vec![9, 0, 0, 0, 0, 0, 0, 0, 3],
-        vec![0, 0, 2, 0, 0, 0, 0, 4, 5],
-        vec![0, 0, 9, 0, 0, 1, 4, 3, 0],
-        vec![3, 0, 1, 7, 0, 0, 0, 0, 9],
-        vec![0, 7, 0, 3, 9, 0, 0, 2, 0],
-    ];
+    let board = read_board();
 
     // solver
-    let board2 = read_board();
 
     for row in board {
         for num in row {
@@ -31,8 +20,6 @@ fn main() {
         }
         println!();
     }
-
-    println!("{:?}", board2);
 }
 
 fn read_board() -> Vec<Vec<i32>> {
